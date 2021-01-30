1f21a447af8bd4c6e5cea000390c8889b9d08cb4
Fdobrovols <Chubays30@gmail.com>
2021-01-30 09:50:31 +0200

    Вторая точка востановления

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println(" Пока C++ ");
+
+	for(int i=0;i<5;i++)
+    {
+        System.out.println(i);
+    }
 	System.out.println(" Привет от Федора");
     }
 }
